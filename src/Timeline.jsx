bf12c2e7242a85dea47ed9074c87c57564ac434f
francetambwe <143693696+francetambwe@users.noreply.github.com>
2024-03-13 11:03:15 +0200

    création du sidebar et ses composants.

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,10 +1,13 @@
 import TweetEditor from "./composants/TweetEditor/TweetEditor.jsx";
 import Header from "./composants/Header/Header.jsx";
 import Tweets from "./composants/Tweets/tweets.jsx";
+import Sidebar from "./sidebar/sidebar.jsx";
+import './style.css';
 function Timeline() {
   return (
     <>
       <div></div>
+      <Sidebar/>
       <div className="timeline">
         <Header />
         <TweetEditor />
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Timeline from "./Timeline.jsx";
 import "./App.css";
+import Sidebar from "./sidebar/sidebar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Timeline />
+
   </React.StrictMode>
 );
